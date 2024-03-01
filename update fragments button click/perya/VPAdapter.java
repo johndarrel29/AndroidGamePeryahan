6f51eb7fb554ed4418bc5d4b1e278346cb95ab39
jarltordrcilla32 <157564175+jarltordrcilla32@users.noreply.github.com>
2024-03-01 23:32:24 +0800

    Add files via upload

---
+++ update fragments button click/perya/VPAdapter.java
@@ -0,0 +1,78 @@
+package com.example.perya;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class VPAdapter extends RecyclerView.Adapter<VPAdapter.ViewHolder> {
+    private final RecyclerViewInterface recyclerViewInterface;
+    Context context;
+    ArrayList<ViewPagerItem> viewPagerItemArrayList;
+
+    public VPAdapter(Context context, ArrayList<ViewPagerItem> viewPagerItemArrayList,
+                     RecyclerViewInterface recyclerViewInterface) {
+        this.context = context;
+        this.viewPagerItemArrayList = viewPagerItemArrayList;
+        this.recyclerViewInterface = recyclerViewInterface;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.viewpager_item, parent, false);
+
+        return new ViewHolder(view, recyclerViewInterface);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        ViewPagerItem viewPagerItem = viewPagerItemArrayList.get(position);
+
+        holder.imageView.setImageResource(viewPagerItem.imageID);
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return viewPagerItemArrayList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageView;
+
+
+        public ViewHolder(@NonNull View itemView, RecyclerViewInterface recyclerViewInterface) {
+            super(itemView);
+
+            imageView = itemView.findViewById(R.id.ivimage);
+            Button playbutton = itemView.findViewById(R.id.playbutton);
+            playbutton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(recyclerViewInterface != null){
+                        int pos = getAdapterPosition();
+
+                        if (pos != RecyclerView.NO_POSITION){
+                            recyclerViewInterface.onItemClick(pos);
+                        }
+                    }
+                }
+            });
+
+            }
+        }
+    }
